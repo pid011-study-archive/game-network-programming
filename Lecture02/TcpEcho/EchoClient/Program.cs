4615096c197bfa0422112aa42af35186b8980ae3
pid011 <pid011@kakao.com>
2021-03-13 22:48:17 +0900

    Add Lecture02 assignments

--- Lecture02/TcpEcho/EchoClient/Program.cs
+++ Lecture02/TcpEcho/EchoClient/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EchoClient
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            IPEndPoint clientAddress = new(IPAddress.Parse("127.0.0.1"), 0);
+            TcpClient client = new(clientAddress);
+
+            try
+            {
+                await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), 9050);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("서버에 연결하지 못했습니다.");
+                return;
+            }
+
+            using NetworkStream networkStream = client.GetStream();
+            using StreamReader reader = new(networkStream, Encoding.UTF8);
+            using StreamWriter writer = new(networkStream, Encoding.UTF8);
+
+            string message;
+
+            try
+            {
+                message = await reader.ReadLineAsync();
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    Console.Write("Input: ");
+                    var input = Console.ReadLine();
+
+                    if (input is "exit") break;
+
+                    await writer.WriteLineAsync(input);
+                    await writer.FlushAsync();
+
+                    message = await reader.ReadLineAsync();
+                    Console.WriteLine($"Output: {message}");
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("서버와의 연결이 끊겼습니다.");
+            }
         }
     }
 }
--- Lecture02/TcpEcho/EchoServer/Program.cs
+++ Lecture02/TcpEcho/EchoServer/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EchoServer
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            IPEndPoint localAddress = new(IPAddress.Parse("127.0.0.1"), 9050);
+            TcpListener server = new(localAddress);
+
+            server.Start();
+
+            var client = await server.AcceptTcpClientAsync();
+            var clientIpEndPoint = client.Client.RemoteEndPoint as IPEndPoint;
+
+            Console.WriteLine($"Connected from {clientIpEndPoint.Address}:{clientIpEndPoint.Port}");
+
+            using NetworkStream networkStream = client.GetStream();
+            using StreamReader reader = new(networkStream, Encoding.UTF8);
+            using StreamWriter writer = new(networkStream, Encoding.UTF8);
+
+            string message;
+
+            try
+            {
+                await writer.WriteLineAsync("Echo서버에 접속하셨습니다.");
+                await writer.FlushAsync();
+
+                while (true)
+                {
+                    message = await reader.ReadLineAsync();
+                    Console.WriteLine($"{clientIpEndPoint.Address}:{clientIpEndPoint.Port}: {message}");
+
+                    await writer.WriteLineAsync(message);
+                    await writer.FlushAsync();
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("클라이언트 연결이 끊김.");
+            }
+
+            Console.WriteLine($"Disconnected from {clientIpEndPoint.Address}:{clientIpEndPoint.Port}");
+            server.Stop();
         }
     }
 }
